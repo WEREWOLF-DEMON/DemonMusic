ad0335f74efa84a68ddd69a5ba7745765a081fdf
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-06-21 22:06:37 +0530

    Update git.py

--- Demon/core/git.py
+++ Demon/core/git.py
@@ -13,17 +13,17 @@ from ..logging import LOGGER
 def install_req(cmd: str) -> Tuple[str, str, int, int]:
     async def install_requirements():
         args = shlex.split(cmd)
-        DEMONcess = await asyncio.create_subDEMONcess_exec(
+        process = await asyncio.create_subprocess_exec(
             *args,
-            stdout=asyncio.subDEMONcess.PIPE,
-            stderr=asyncio.subDEMONcess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
         )
-        stdout, stderr = await DEMONcess.communicate()
+        stdout, stderr = await process.communicate()
         return (
             stdout.decode("utf-8", "replace").strip(),
             stderr.decode("utf-8", "replace").strip(),
-            DEMONcess.returncode,
-            DEMONcess.pid,
+            process.returncode,
+            process.pid,
         )
 
     return asyncio.get_event_loop().run_until_complete(install_requirements())
