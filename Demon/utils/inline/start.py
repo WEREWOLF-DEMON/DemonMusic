b4a790897236229ff2df741afa7cfb43d452cc4f
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-06-21 23:57:17 +0530

    Update start.py

--- Demon/utils/inline/start.py
+++ Demon/utils/inline/start.py
@@ -45,6 +45,13 @@ def private_panel(_):
 def private_panell(_):
     buttons = [
         [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
@@ -52,8 +59,6 @@ def private_panell(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
