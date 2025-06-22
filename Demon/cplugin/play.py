8f599999e0de2ee42f2ef1439951cf0e44d650f0
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2025-06-22 12:27:09 +0530

    Update play.py

--- Demon/cplugin/play.py
+++ Demon/cplugin/play.py
@@ -1,6 +1,8 @@
 import os
 import random
 import string
+from Demon.utils.database.clonedb import is_bot_verified
+from Demon import app
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
@@ -45,6 +47,14 @@ SPAM_THRESHOLD = 2
 SPAM_WINDOW_SECONDS = 5
 
 
+
+@app.on_message(filters.command("play"))
+async def play_music(client, message):
+    bot_id = (await client.get_me()).id
+    if not is_bot_verified(bot_id):
+        return await message.reply_text("⛔ Ye cloned bot abhi verify nahi hua hai. Kripya @WerewolfDemon se verify karwaayein.")
+    
+    # continue normally...
 @Client.on_message(
     filters.command(
         [
