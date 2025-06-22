e17a1b791757f5f62fa562347236ed357981d297
WEREWOLF-DEMON <153902241+WEREWOLF-DEMON@users.noreply.github.com>
2025-06-22 09:39:23 +0530

    ***

--- Demon/utils/inline/queue.py
+++ Demon/utils/inline/queue.py
@@ -105,7 +105,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/DEMONBotts",
+                url="https://t.me/WerewolfDemonUpdate",
             ),
         ],
     ]
